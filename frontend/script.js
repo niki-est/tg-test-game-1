55c4a1709a4485838128381749203fdad37b60cc
Niki Est <niki.est@yandex.ru>
2025-03-08 02:01:56 +0300

    Edited front script

--- frontend/script.js
+++ frontend/script.js
@@ -6,16 +6,3 @@ ctx.font = "16px Arial";
 ctx.fillText("Сцена 1: Ты стоишь перед военкоматом.", 20, 50);
 ctx.fillText("Что делать?", 20, 80);
 
-function chooseOption(choice) {
-  if (window.TelegramGameProxy) {
-    TelegramGameProxy.sendMessage(choice); // Отправка выбора боту
-  } else {
-    console.log("Выбор: " + choice); // Для теста вне Telegram
-  }
-}
-
-// Инициализация Telegram Games
-if (window.TelegramGameProxy) {
-  TelegramGameProxy.init();
-  console.log("Игра запущена в Telegram!");
-}
