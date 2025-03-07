0a24089313537e49cff75a82dc2a37c3783af492
Niki Est <niki.est@yandex.ru>
2025-03-07 03:58:56 +0300

    Переписал апи

--- backend/api/hello.js
+++ backend/api/hello.js
@@ -1,3 +1,12 @@
-export default function handler(req, res) {
-    res.status(200).json({ message: "Привет, это твой API на Vercel!" });
-}
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/api/hello", (req, res) => {
+    res.json({ message: "Привет, это твой API на Vercel!" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
